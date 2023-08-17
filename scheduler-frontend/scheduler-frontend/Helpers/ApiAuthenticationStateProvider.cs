dcac07d228411c96059d6643e3ddbe5f98735676
balogunpraise <balogunpraise2@gmail.com>
2023-08-17 16:06:42 +0100

    created login helper

--- scheduler-frontend/scheduler-frontend/Helpers/ApiAuthenticationStateProvider.cs
+++ scheduler-frontend/scheduler-frontend/Helpers/ApiAuthenticationStateProvider.cs
@@ -1,7 +1,82 @@
-﻿namespace schedulerfrontend.Helpers
-{
-    public class ApiAuthenticationStateProvider
-    {
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
 
+namespace schedulerfrontend.Helpers
+{
+    public class ApiAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorageService;
+
+        public ApiAuthenticationStateProvider(HttpClient httpClient, ILocalStorageService localStorageService)
+        {
+            _httpClient = httpClient;
+            _localStorageService = localStorageService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var savedToken = await _localStorageService.GetItemAsync<string>("authToken");
+            if(string.IsNullOrWhiteSpace(savedToken))
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", savedToken);
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
+        }
+
+        public void MarkUserAsAuthenticated(string email)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, email) }, "apiauth"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            var annonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+            var authState = Task.FromResult(new AuthenticationState(annonymousUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var claims = new List<Claim>();
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            keyValuePairs!.TryGetValue(ClaimTypes.Role, out object roles);
+            if(roles != null)
+            {
+                if (roles.ToString()!.Trim().StartsWith("["))
+                {
+                    var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString()!);
+                    foreach(var pasedRole in parsedRoles!)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, pasedRole));
+                    }
+                }
+                else
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, roles.ToString()!));
+                }
+                keyValuePairs.Remove(ClaimTypes.Role);
+            }
+            claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()!)));
+            return claims;
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch(base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
     }
 }
