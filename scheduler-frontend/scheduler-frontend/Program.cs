9b1335e1d09fc299f062e975640c8431df562bba
balogunpraise <balogunpraise2@gmail.com>
2023-08-17 23:02:49 +0100

    add login logic on the view

--- scheduler-frontend/scheduler-frontend/Program.cs
+++ scheduler-frontend/scheduler-frontend/Program.cs
@@ -1,10 +1,18 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using schedulerfrontend;
+using schedulerfrontend.Helpers;
+using schedulerfrontend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
