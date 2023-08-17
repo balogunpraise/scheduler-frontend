f1775126dd48cc601e3f225bb216e4c2fd22951a
balogunpraise <balogunpraise2@gmail.com>
2023-08-17 20:33:22 +0100

    add login service

--- scheduler-frontend/scheduler-frontend/Services/AuthService.cs
+++ scheduler-frontend/scheduler-frontend/Services/AuthService.cs
@@ -1,7 +1,11 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Models.Authentication;
+using schedulerfrontend.Helpers;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 
 namespace schedulerfrontend.Services
 {
@@ -26,9 +30,20 @@ namespace schedulerfrontend.Services
             }
             return new RegisterResult { Successful = true, Errors = new List<string>() { "Account Created successfully " } };
         }
-        public Task<LoginResult> Login(LoginModel loginModel)
+        public async Task<LoginResult> Login(LoginModel loginModel)
         {
-            throw new NotImplementedException();
+            var loginAsJson = JsonSerializer.Serialize(loginModel);
+            var response = await _httpClient.PostAsync("api/login", new StringContent(loginAsJson, Encoding.UTF8, "application/json"));
+            var loginResult = JsonSerializer.Deserialize<LoginResult>(await response.Content.ReadAsStringAsync(), 
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+            if (!response.IsSuccessStatusCode)
+            {
+                return loginResult!;
+            }
+            await _localStorageService.SetItemAsync("authToken", loginResult!.Token);
+            ((ApiAuthenticationStateProvider)_stateProvider).MarkUserAsAuthenticated(loginModel.Email!);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", loginResult.Token);
+            return loginResult;
         }
 
         public Task Logout()
